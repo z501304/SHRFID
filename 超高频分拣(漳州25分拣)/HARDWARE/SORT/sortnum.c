c381d78fa44697b6d682a8583e12acc67a192cc0
dearLong <501304708@qq.com>
2023-06-07 13:24:56 +0800

    漳州25分拣

---
+++ 超高频分拣(漳州25分拣)/HARDWARE/SORT/sortnum.c
@@ -0,0 +1,15 @@
+/******************************************************************************************
+ * @FilePath: sortnum.c
+ * @Author: jack
+ * @Date: 2023-06-07 12:14:25
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2023-06-07 12:16:09
+ * @Copyright: 2023 xxxTech CO.,LTD. All Rights Reserved.
+ * @Descripttion: 
+******************************************************************************************/
+#include "sortnum.h"
+void sort(u8 sortnun)
+{
+
+    
+}
---
+++ 超高频分拣(漳州25分拣)/USER/sortnum.h
@@ -0,0 +1,6 @@
+#ifndef _SORTNUM_
+#define _SORTNUM_
+#include"stm32f10x.h"
+void sort(u8 sotnum);
+
+#endif // !_SORTNUM_
