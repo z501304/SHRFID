e35aaf9dba37642fcbc4c0aec8033dd857d6e1ad
dearLong <501304708@qq.com>
2023-06-07 13:49:36 +0800

    欣中复旦全向轮分拣

---
+++ 馆员还书机(欣中全向轮分拣)/include.h
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------
+//   PROG : 
+//   DATE : 2013.12
+//   COMP : sh-rfid
+//   DESP : all file list
+//---------------------------------------------------------------------
+
+#ifndef _INCLUDE_H_
+#define _INCLUDE_H_
+
+#include <reg52.h>
+#include <string.h>
+#include "equipment.h"
+#include "inteface.h"
+#include "system_initiate.h"
+#include "communication.h"
+
+#include "MyDataType.h"
+#define  version_H		2 
+#define  version_L		0 			 //¹Ì¼þ°æ±¾ºÅV2.0
+
+//extern void WatchDog(void);
+#endif
